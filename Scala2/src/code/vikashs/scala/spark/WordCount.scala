1541c27b60a67f29f5dcc567b5e2dd6c010dc8fd
Vikash Singh <imvikashsingh@outlook.com>
2018-07-29 11:51:07 +0530

    done

--- Scala2/src/code/vikashs/scala/spark/WordCount.scala
+++ Scala2/src/code/vikashs/scala/spark/WordCount.scala
@@ -1,5 +1,15 @@
 package code.vikashs.scala.spark
+import org.apache.spark._
 
-class WordCount {
-  
+object WordCount 
+{
+  def main(args: Array[String]): Unit = {
+      val sc = new SparkContext("local[*]","TminByLocation")
+      val lines=sc.textFile("G://Workspace//ScalaWorkSpace47//book.txt")
+      
+      val dataMap=lines.flatMap(x => x.split(" "))
+      
+      val wordCounts=dataMap.countByValue()
+      wordCounts.foreach(println)
+  }
 }
