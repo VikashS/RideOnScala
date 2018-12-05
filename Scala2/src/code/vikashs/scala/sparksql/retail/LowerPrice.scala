b27adbeee1b22d7a1d56a5aefbba63c163ef1bef
Vikash Singh <imvikashsingh@outlook.com>
2018-12-05 07:39:05 +0530

    Update LowerPrice.scala

--- Scala2/src/code/vikashs/scala/sparksql/retail/LowerPrice.scala
+++ Scala2/src/code/vikashs/scala/sparksql/retail/LowerPrice.scala
@@ -23,14 +23,14 @@ object LowerPrice {
     .option("header", "true").
     option("delimiter","|")
     .option("inferSchema", "true") 
-    .load("G://Workspace//ScalaWorkSpace47//ecom//ecom_competitor_data.txt")
+    .load("G://Workspace//ScalaWorkSpace47//data//first.txt")
 
   val internalProductData=sqlContext.read
     .format("com.databricks.spark.csv")
     .option("header", "true").
     option("delimiter","|")
     .option("inferSchema", "true") 
-    .load("G://Workspace//ScalaWorkSpace47//ecom//internal_product_data.txtt")
+    .load("G://Workspace//ScalaWorkSpace47//data//second.txt")
 
 
   val sellerData=sqlContext.read
@@ -38,32 +38,8 @@ object LowerPrice {
     .option("header", "true").
     option("delimiter","|")
     .option("inferSchema", "true") // Automatically infer data types
-    .load("G://Workspace//ScalaWorkSpace47//ecom//seller_data.txt")
+    .load("G://Workspace//ScalaWorkSpace47//edata//third.txt")
     
     val sellerMap: Map[Int, String] =sellerData.map(row => (row.getAs[Int]("SellerID"),row.getAs[String]("netValue"))).collect().toMap
     val sellerBroadcast=sc.broadcast(sellerMap)
-   /* 
-    val minPriceRDD = compititorData.map(row => (row.getAs[Int]("productId"),row ) ).reduceByKey
-    {
-    (row1,row2) =>
-      if(row1.getAs[Double]("price")<  row2.getAs[Double]("price"))
-        row1
-      else
-        row2
-  }*/
-
-   /* val internalRDD: RDD[(Int, Row)] = internalProductData.map(row => (row.getAs[Int]("ProductId"), row))
-    val rdd: RDD[(Int, (Row, Row))] = internalRDD.join(minPriceRDD)
-    rdd.map {
-      case (productID, (row1, row2)) =>
-        val description = sellerBroadcast.value.getOrElse(row1.getAs[Int]("SellerID"), "")
-        MagicPriceData(productID, row2.getAs[Double]("price"), "", row2.getAs[Double]("price"), row2.getAs[String]("rivalName"), description)
-    }.toDF().show()
-*/
-      
-   
-  }
-
-}
-
-case class MagicPriceData(productid:Int, msprice:Double, timestamp:String, cheapestprice:Double, rivalname:String,sellerdescription:String)
+  
